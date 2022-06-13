1ac98506a6461ed7096d42d6218547cd362b390d
twdeveloper <twdeveloper@outlook.com>
2022-06-13 23:34:30 +0800

    使用NSubstitute建立Fake物件

--- ConsoleApp1Tests/FinancyTests.cs
+++ ConsoleApp1Tests/FinancyTests.cs
@@ -3,6 +3,7 @@ using ConsoleApp1;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using NSubstitute;
 
 namespace ConsoleApp1.Tests
 {
@@ -12,8 +13,13 @@ namespace ConsoleApp1.Tests
         [TestMethod()]
         public void SplitMoneyTest()
         {
+            //使用NSubstitute建立Fake物件
+            var FakeCurrencyConverter = NSubstitute.Substitute.For<ICurrencyConverter>();
+            //設定預計回傳值
+            FakeCurrencyConverter.Convert("USD", "TWD").Returns(27.67222F);
+
             //注入測試用類別實作
-            Financy f = new Financy(new FakeCurrencyConverter());
+            Financy f = new Financy(FakeCurrencyConverter);
             var CostByPeople = f.SplitMoney(100, 5);
             Assert.IsTrue(CostByPeople.ToString().StartsWith("553.444"));
         }
